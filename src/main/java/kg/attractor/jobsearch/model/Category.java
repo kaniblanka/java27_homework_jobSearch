a4691ca4ed0f8cc7a1ce02edba1894c7fe71544d
Kaniblanka <kanyshai.sydygalieva01@gmail.com>
2026-03-12 17:03:21 +0600

    created Category data model for job search

---
+++ src/main/java/kg/attractor/jobsearch/model/Category.java
@@ -0,0 +1,16 @@
+package kg.attractor.jobsearch.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Category {
+
+    private Long id;
+    private String name;
+    private Long parentId;
+
+}
