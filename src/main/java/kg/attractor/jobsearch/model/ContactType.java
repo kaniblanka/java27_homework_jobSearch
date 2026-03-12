811a71aa901424c0e400a7100342ef5509a8b986
Kaniblanka <kanyshai.sydygalieva01@gmail.com>
2026-03-12 17:04:49 +0600

    created ContactType data model for job search

---
+++ src/main/java/kg/attractor/jobsearch/model/ContactType.java
@@ -0,0 +1,15 @@
+package kg.attractor.jobsearch.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class ContactType {
+
+    private Long id;
+    private String type;
+
+}
