52b1196c6b6773a3c442b727aaea0186fa7bdae0
Kaniblanka <kanyshai.sydygalieva01@gmail.com>
2026-03-12 17:02:14 +0600

    created Resume data model for job search

---
+++ src/main/java/kg/attractor/jobsearch/model/Resume.java
@@ -0,0 +1,23 @@
+package kg.attractor.jobsearch.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Resume {
+
+    private Long id;
+    private Long applicantId;
+    private String name;
+    private Long categoryId;
+    private Double salary;
+    private Boolean isActive;
+    private LocalDateTime createdDate;
+    private LocalDateTime updateTime;
+
+}
