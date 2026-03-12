82d845dd52f237b518924b44e98b077850d2bb4c
Kaniblanka <kanyshai.sydygalieva01@gmail.com>
2026-03-12 16:56:47 +0600

    created User data model for job search

---
+++ src/main/java/kg/attractor/jobsearch/model/User.java
@@ -0,0 +1,22 @@
+package kg.attractor.jobsearch.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+
+    private Long id;
+    private String name;
+    private String surname;
+    private Integer age;
+    private String email;
+    private String password;
+    private String phoneNumber;
+    private String avatar;
+    private String accountType;
+
+}
