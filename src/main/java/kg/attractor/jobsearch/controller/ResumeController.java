73d90c12c25e18a771859a1cdbb5fc101bdf18c3
Kaniblanka <kanyshai.sydygalieva01@gmail.com>
2026-03-18 23:28:15 +0600

    created controller for resumer

---
+++ src/main/java/kg/attractor/jobsearch/controller/ResumeController.java
@@ -0,0 +1,36 @@
+package kg.attractor.jobsearch.controller;
+
+import kg.attractor.jobsearch.model.Resume;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("resumes")
+public class ResumeController {
+
+    @PostMapping
+    public HttpStatus createResume(@RequestBody Resume resume) {
+        return HttpStatus.OK;
+    }
+
+    @PutMapping("{id}")
+    public HttpStatus updateResume(@PathVariable Long id,
+                                   @RequestBody Resume resume) {
+        return HttpStatus.OK;
+    }
+
+    @DeleteMapping("{id}")
+    public HttpStatus deleteResume(@PathVariable Long id) {
+        return HttpStatus.OK;
+    }
+
+    @GetMapping
+    public HttpStatus getAllResumes() {
+        return HttpStatus.OK;
+    }
+
+    @GetMapping("category/{category}")
+    public HttpStatus getResumesByCategory(@PathVariable String category) {
+        return HttpStatus.OK;
+    }
+}
