78e299914fb7087c4cb2b63290febad08e189ae3
Kaniblanka <kanyshai.sydygalieva01@gmail.com>
2026-03-18 20:36:51 +0600

    created VacancyController

---
+++ src/main/java/kg/attractor/jobsearch/controller/VacancyController.java
@@ -0,0 +1,38 @@
+package kg.attractor.jobsearch.controller;
+
+import kg.attractor.jobsearch.model.Vacancy;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("vacancies")
+@RequiredArgsConstructor
+public class VacancyController {
+
+    @PostMapping
+    public HttpStatus createVacancy(@RequestBody Vacancy vacancy) {
+        return HttpStatus.OK;
+    }
+
+    @PutMapping("{id}")
+    public HttpStatus updateVacancy(@PathVariable Long id,
+                                    @RequestBody Vacancy vacancy) {
+        return HttpStatus.OK;
+    }
+
+    @DeleteMapping("{id}")
+    public HttpStatus deleteVacancy(@PathVariable Long id) {
+        return HttpStatus.OK;
+    }
+
+    @GetMapping
+    public HttpStatus getAllActiveVacancies() {
+        return HttpStatus.OK;
+    }
+
+    @GetMapping("category")
+    public HttpStatus getVacanciesByCategory(@RequestParam String category) {
+        return HttpStatus.OK;
+    }
+}
