4ddda84fc699ee79eae041a58f035c0939476d9f
Kaniblanka <kanyshai.sydygalieva01@gmail.com>
2026-03-19 18:39:42 +0600

    rebuild UserController

--- src/main/java/kg/attractor/jobsearch/controller/UserController.java
+++ src/main/java/kg/attractor/jobsearch/controller/UserController.java
@@ -1,27 +1,36 @@
 package kg.attractor.jobsearch.controller;
 
 import kg.attractor.jobsearch.model.User;
+import kg.attractor.jobsearch.service.UserService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("users")
 @RequiredArgsConstructor
 public class UserController {
 
-    @PostMapping
-    public HttpStatus createUser(@RequestBody User user) {
-        return HttpStatus.OK;
+    private final UserService userService;
+
+    @GetMapping
+    public List<User> getAllUsers() {
+        return userService.getAllUsers();
     }
 
-    @GetMapping("job-seeker/{id}")
-    public HttpStatus getJobSeeker(@PathVariable Long id) {
-        return HttpStatus.OK;
+    @GetMapping("{id}")
+    public User findById(@PathVariable Long id) {
+        return userService.findById(id);
     }
 
-    @GetMapping("employer/{id}")
-    public HttpStatus getEmployer(@PathVariable Long id) {
-        return HttpStatus.OK;
+    @GetMapping("email")
+    public User findByEmail(@RequestParam String email) {
+        return userService.findByEmail(email);
+    }
+
+    @GetMapping("exists")
+    public boolean existsByEmail(@RequestParam String email) {
+        return userService.existsByEmail(email);
     }
 }
