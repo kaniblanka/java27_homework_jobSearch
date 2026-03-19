0f1911f5d64dee0cd1a2d82457f52782f43802e8
Kaniblanka <kanyshai.sydygalieva01@gmail.com>
2026-03-19 18:37:24 +0600

    created UserService and UserServiceImpl

--- src/main/java/kg/attractor/jobsearch/service/UserService.java
+++ src/main/java/kg/attractor/jobsearch/service/UserService.java
@@ -1,37 +1,12 @@
 package kg.attractor.jobsearch.service;
 
 import kg.attractor.jobsearch.model.User;
-import org.springframework.stereotype.Service;
 
-import java.util.Optional;
+import java.util.List;
 
-@Service
-public class UserService {
-
-    public User createUser(User user) {
-        // тут регистрируем пользователя
-        // приходит объект user с фронта
-        // потом он должен сохраниться
-        return user;
-    }
-
-    public Optional<User> getJobSeekerById(Long id) {
-        // ищем соискателя по id
-        // если нашли возвращаем, если нет то empty
-        return Optional.empty();
-    }
-
-    public Optional<User> getEmployerById(Long id) {
-        // ищем работодателя по id
-        // логика такая же как и сверху
-        return Optional.empty();
-    }
-
-    public Optional<User> login(String email, String password) {
-        // тут должна быть авторизация
-        // проверяем email и пароль
-        // если все ок, возвращаем пользователя
-        // если нет, пусто
-        return Optional.empty();
-    }
+public interface UserService {
+    List<User> getAllUsers();
+    User findById(Long id);
+    User findByEmail(String email);
+    boolean existsByEmail(String email);
 }
---
+++ src/main/java/kg/attractor/jobsearch/service/impl/UserServiceImpl.java
@@ -0,0 +1,39 @@
+package kg.attractor.jobsearch.service.impl;
+
+import kg.attractor.jobsearch.dao.UserDao;
+import kg.attractor.jobsearch.exception.UserNotFoundException;
+import kg.attractor.jobsearch.model.User;
+import kg.attractor.jobsearch.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class UserServiceImpl implements UserService {
+
+    private final UserDao userDao;
+
+    @Override
+    public List<User> getAllUsers() {
+        return userDao.findAll();
+    }
+
+    @Override
+    public User findById(Long id) {
+        return userDao.findById(id)
+                .orElseThrow(UserNotFoundException::new);
+    }
+
+    @Override
+    public User findByEmail(String email) {
+        return userDao.findByEmail(email)
+                .orElseThrow(UserNotFoundException::new);
+    }
+
+    @Override
+    public boolean existsByEmail(String email) {
+        return userDao.findByEmail(email).isPresent();
+    }
+}
