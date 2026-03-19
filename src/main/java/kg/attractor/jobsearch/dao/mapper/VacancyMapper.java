7addbcd34c08596f7e2b2af3d97f9bcbeca446ad
Kaniblanka <kanyshai.sydygalieva01@gmail.com>
2026-03-19 19:24:33 +0600

    changed VacancyMapper

--- src/main/java/kg/attractor/jobsearch/dao/mapper/VacancyMapper.java
+++ src/main/java/kg/attractor/jobsearch/dao/mapper/VacancyMapper.java
@@ -5,16 +5,29 @@ import org.springframework.jdbc.core.RowMapper;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
-
-
 public class VacancyMapper implements RowMapper<Vacancy> {
+
     @Override
     public Vacancy mapRow(ResultSet rs, int rowNum) throws SQLException {
         Vacancy v = new Vacancy();
+
         v.setId(rs.getLong("id"));
-        v.setName(rs.getString("title"));
+        v.setName(rs.getString("name"));
         v.setDescription(rs.getString("description"));
+
         v.setCategoryId(rs.getLong("category_id"));
+        v.setSalary(rs.getDouble("salary"));
+
+        v.setExpFrom(rs.getInt("exp_from"));
+        v.setExpTo(rs.getInt("exp_to"));
+
+        v.setIsActive(rs.getBoolean("is_active"));
+
+        v.setAuthorId(rs.getLong("author_id"));
+
+        v.setCreatedDate(rs.getTimestamp("created_date").toLocalDateTime());
+        v.setUpdateTime(rs.getTimestamp("update_time").toLocalDateTime());
+
         return v;
     }
 }
