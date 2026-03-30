4a3c81450a14dbfd4b31eb613badde2ae4d4f5f4
Kaniblanka <kanyshai.sydygalieva01@gmail.com>
2026-03-30 17:34:28 +0600

    created interface RespondedApplicantService

--- src/main/java/kg/attractor/jobsearch/service/RespondedApplicantService.java
+++ src/main/java/kg/attractor/jobsearch/service/RespondedApplicantService.java
@@ -1,22 +1,12 @@
 package kg.attractor.jobsearch.service;
 
 import kg.attractor.jobsearch.model.RespondedApplicant;
-import org.springframework.stereotype.Service;
 
 import java.util.List;
 
-@Service
-public class RespondedApplicantService {
+public interface RespondedApplicantService {
 
-    public RespondedApplicant respondToVacancy(RespondedApplicant respondedApplicant) {
-        // тут соискатель откликается на вакансию
-        // сохраняем кто откликнулся и на какую вакансию
-        return respondedApplicant;
-    }
+    RespondedApplicant respondToVacancy(RespondedApplicant respondedApplicant);
 
-    public List<RespondedApplicant> getRespondedApplicantsByVacancyId(Long vacancyId) {
-        // получаем всех кто откликнулся на вакансию
-        // по id вакансии
-        return List.of();
-    }
+    List<RespondedApplicant> getRespondedApplicantsByVacancyId(Long vacancyId);
 }
