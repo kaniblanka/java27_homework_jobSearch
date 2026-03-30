caf0de0fe38de0a2409f2c000cd4b80a949a4d6c
Kaniblanka <kanyshai.sydygalieva01@gmail.com>
2026-03-30 17:37:05 +0600

    changed RespondedApplicantDao

--- src/main/java/kg/attractor/jobsearch/dao/RespondedApplicantDao.java
+++ src/main/java/kg/attractor/jobsearch/dao/RespondedApplicantDao.java
@@ -5,9 +5,12 @@ import kg.attractor.jobsearch.model.RespondedApplicant;
 import lombok.RequiredArgsConstructor;
 import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.jdbc.support.GeneratedKeyHolder;
+import org.springframework.jdbc.support.KeyHolder;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
+import java.util.Objects;
 
 @Component
 @RequiredArgsConstructor
@@ -15,14 +18,19 @@ public class RespondedApplicantDao {
 
     private final NamedParameterJdbcTemplate jdbcTemplate;
 
-    public List<RespondedApplicant> findByResumeId(Long resumeId) {
-        String sql = "select * from responses where resume_id = :resumeId";
+    public Long create(RespondedApplicant respondedApplicant) {
+        String sql = "insert into responses(resume_id, vacancy_id, confirmation) " +
+                "values (:resumeId, :vacancyId, :confirmation)";
 
-        return jdbcTemplate.query(
-                sql,
-                new MapSqlParameterSource().addValue("resumeId", resumeId),
-                new RespondedApplicantMapper()
-        );
+        MapSqlParameterSource params = new MapSqlParameterSource()
+                .addValue("resumeId", respondedApplicant.getResumeId())
+                .addValue("vacancyId", respondedApplicant.getVacancyId())
+                .addValue("confirmation", respondedApplicant.getConfirmation());
+
+        KeyHolder keyHolder = new GeneratedKeyHolder();
+        jdbcTemplate.update(sql, params, keyHolder);
+
+        return Objects.requireNonNull(keyHolder.getKey()).longValue();
     }
 
     public List<RespondedApplicant> findByVacancyId(Long vacancyId) {
