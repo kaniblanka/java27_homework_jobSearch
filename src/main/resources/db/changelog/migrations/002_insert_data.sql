2e3fc7db690a793e4c0aed3743b9ccbbaad9cfc6
Kaniblanka <kanyshai.sydygalieva01@gmail.com>
2026-03-30 18:03:10 +0600

    added subqueries for Ids from related tables

--- src/main/resources/db/changelog/migrations/002_insert_data.sql
+++ src/main/resources/db/changelog/migrations/002_insert_data.sql
@@ -15,17 +15,61 @@ VALUES
 
 INSERT INTO resumes(applicant_id, name, category_id, salary, is_active, created_date, update_time)
 VALUES
-    (1, 'Java Developer', 1, 1200, true, now(), now()),
-    (1, 'Frontend Developer', 1, 1000, true, now(), now());
+    (
+        (select id from users where email = 'aida@mail.com'),
+        'Java Developer',
+        (select id from categories where name = 'IT'),
+        1200,
+        true,
+        now(),
+        now()
+    ),
+    (
+        (select id from users where email = 'aida@mail.com'),
+        'Frontend Developer',
+        (select id from categories where name = 'IT'),
+        1000,
+        true,
+        now(),
+        now()
+    );
 
 INSERT INTO vacancies(name, description, category_id, salary, exp_from, exp_to, is_active, author_id, created_date, update_time)
 VALUES
-    ('Backend Dev', 'Spring Boot dev', 1, 1500, 1, 3, true, 2, now(), now()),
-    ('UI Designer', 'Figma designer', 2, 1200, 1, 2, true, 2, now(), now());
+    (
+        'Backend Dev',
+        'Spring Boot dev',
+        (select id from categories where name = 'IT'),
+        1500,
+        1,
+        3,
+        true,
+        (select id from users where email = 'bek@mail.com'),
+        now(),
+        now()
+    ),
+    (
+        'UI Designer',
+        'Figma designer',
+        (select id from categories where name = 'Design'),
+        1200,
+        1,
+        2,
+        true,
+        (select id from users where email = 'bek@mail.com'),
+        now(),
+        now()
+    );
 
 INSERT INTO responses(resume_id, vacancy_id, confirmation)
 VALUES
-    (1, 1, true);
+    (
+        (select id from resumes where name = 'Java Developer'
+                                  and applicant_id = (select id from users where email = 'aida@mail.com')),
+        (select id from vacancies where name = 'Backend Dev'
+                                    and author_id = (select id from users where email = 'bek@mail.com')),
+        true
+    );
 
 INSERT INTO authorities(authority)
 VALUES
@@ -34,5 +78,11 @@ VALUES
 
 INSERT INTO user_authorities(user_id, authority_id)
 VALUES
-    (1, 1),
-    (2, 2);
+    (
+        (select id from users where email = 'aida@mail.com'),
+        (select id from authorities where authority = 'CREATE_RESUME')
+    ),
+    (
+        (select id from users where email = 'bek@mail.com'),
+        (select id from authorities where authority = 'CREATE_VACANCY')
+    );
