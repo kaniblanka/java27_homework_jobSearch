b4f316bae5d3e23a084a968ef7482bb3ea107474
Kaniblanka <kanyshai.sydygalieva01@gmail.com>
2026-04-09 17:56:09 +0600

    create VacancyCreateWebDto

---
+++ src/main/java/kg/attractor/jobsearch/dto/VacancyCreateWebDto.java
@@ -0,0 +1,35 @@
+package kg.attractor.jobsearch.dto;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.PositiveOrZero;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class VacancyCreateWebDto {
+
+    @NotBlank(message = "Vacancy name cannot be empty")
+    private String name;
+
+    @NotBlank(message = "Description cannot be empty")
+    private String description;
+
+    @NotNull(message = "Category is required")
+    private Long categoryId;
+
+    @NotNull(message = "Salary is required")
+    @PositiveOrZero(message = "Salary must be >= 0")
+    private Double salary;
+
+    @NotNull(message = "Experience from is required")
+    @PositiveOrZero
+    private Integer expFrom;
+
+    @NotNull(message = "Experience to is required")
+    @PositiveOrZero
+    private Integer expTo;
+}
