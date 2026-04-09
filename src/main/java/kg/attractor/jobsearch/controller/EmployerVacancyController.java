3ce29702ca03fb80a36b8c9c34cab019a26a19a4
Kaniblanka <kanyshai.sydygalieva01@gmail.com>
2026-04-09 17:57:01 +0600

    edit EmployerVacancyController

--- src/main/java/kg/attractor/jobsearch/controller/EmployerVacancyController.java
+++ src/main/java/kg/attractor/jobsearch/controller/EmployerVacancyController.java
@@ -2,6 +2,7 @@ package kg.attractor.jobsearch.controller;
 
 import jakarta.validation.Valid;
 import kg.attractor.jobsearch.dto.UserDto;
+import kg.attractor.jobsearch.dto.VacancyCreateWebDto;
 import kg.attractor.jobsearch.dto.VacancyDto;
 import kg.attractor.jobsearch.exception.CreateEntryException;
 import kg.attractor.jobsearch.exception.UserNotFoundException;
@@ -12,6 +13,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -28,26 +30,30 @@ public class EmployerVacancyController {
 
     @GetMapping("create")
     public String createVacancyPage(Model model) {
-        VacancyDto vacancyDto = new VacancyDto();
-        vacancyDto.setIsActive(true);
-
-        model.addAttribute("vacancyDto", vacancyDto);
+        model.addAttribute("vacancyCreateWebDto", new VacancyCreateWebDto());
         return "vacancies/create";
     }
 
     @PostMapping("create")
-    public String createVacancy(@Valid VacancyDto vacancyDto,
+    public String createVacancy(@Valid @ModelAttribute VacancyCreateWebDto vacancyCreateWebDto,
                                 BindingResult bindingResult,
                                 Principal principal,
                                 Model model) throws UserNotFoundException, CreateEntryException {
 
         if (bindingResult.hasErrors()) {
-            model.addAttribute("vacancyDto", vacancyDto);
+            model.addAttribute("vacancyCreateWebDto", vacancyCreateWebDto);
             return "vacancies/create";
         }
 
         UserDto currentUser = userService.findByEmail(principal.getName());
 
+        VacancyDto vacancyDto = new VacancyDto();
+        vacancyDto.setName(vacancyCreateWebDto.getName());
+        vacancyDto.setDescription(vacancyCreateWebDto.getDescription());
+        vacancyDto.setCategoryId(vacancyCreateWebDto.getCategoryId());
+        vacancyDto.setSalary(vacancyCreateWebDto.getSalary());
+        vacancyDto.setExpFrom(vacancyCreateWebDto.getExpFrom());
+        vacancyDto.setExpTo(vacancyCreateWebDto.getExpTo());
         vacancyDto.setAuthorId(currentUser.getId());
         vacancyDto.setIsActive(true);
         vacancyDto.setCreatedDate(LocalDateTime.now());
