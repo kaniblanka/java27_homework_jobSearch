b49ce9130b6ed6330f0954e001a5e000ad91cc29
Kaniblanka <kanyshai.sydygalieva01@gmail.com>
2026-04-21 16:32:20 +0600

    refactor: convert ContactType to JPA entity

--- src/main/java/kg/attractor/jobsearch/model/ContactType.java
+++ src/main/java/kg/attractor/jobsearch/model/ContactType.java
@@ -1,15 +1,25 @@
 package kg.attractor.jobsearch.model;
 
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.util.List;
+
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@Entity
+@Table(name = "contact_types")
 public class ContactType {
-
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(name = "type", nullable = false, unique = true)
     private String type;
 
+    @OneToMany(mappedBy = "type")
+    private List<ContactInfo> contactInfoList;
 }
