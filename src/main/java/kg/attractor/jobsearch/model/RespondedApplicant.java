e9e738c2c7b49c421db1ff5eaa704891d23a04a1
Kaniblanka <kanyshai.sydygalieva01@gmail.com>
2026-04-21 16:36:06 +0600

    refactor: add messages relation to RespondedApplicant

--- src/main/java/kg/attractor/jobsearch/model/RespondedApplicant.java
+++ src/main/java/kg/attractor/jobsearch/model/RespondedApplicant.java
@@ -1,17 +1,11 @@
 package kg.attractor.jobsearch.model;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+import java.util.List;
 
 @Data
 @NoArgsConstructor
@@ -33,4 +27,7 @@ public class RespondedApplicant {
 
     @Column(name = "confirmation")
     private Boolean confirmation;
+
+    @OneToMany(mappedBy = "response")
+    private List<Message> messages;
 }
