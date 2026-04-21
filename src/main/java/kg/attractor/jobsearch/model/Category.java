cde622ce180bb56c176205ee80de826b8c6ce0b0
Kaniblanka <kanyshai.sydygalieva01@gmail.com>
2026-04-21 16:08:07 +0600

    refactor: update Category entity with self-referencing relationship

--- src/main/java/kg/attractor/jobsearch/model/Category.java
+++ src/main/java/kg/attractor/jobsearch/model/Category.java
@@ -1,16 +1,35 @@
 package kg.attractor.jobsearch.model;
 
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.util.List;
+
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@Entity
+@Table(name = "categories")
 public class Category {
-
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    private String name;
-    private Long parentId;
 
+    @Column(name = "name")
+    private String name;
+
+    @OneToMany(mappedBy = "category", fetch = FetchType.EAGER)
+    private List<Resume> resumes;
+
+    @OneToMany(mappedBy = "category", fetch = FetchType.EAGER)
+    private List<Vacancy> vacancies;
+
+    @ManyToOne
+    @JoinColumn(name = "parent_id")
+    private Category parent;
+
+    @OneToMany(mappedBy = "parent")
+    private List<Category> children;
 }
