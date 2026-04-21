06cf9a67511b7b8cb1a5548725f160b49a3b1f3c
Kaniblanka <kanyshai.sydygalieva01@gmail.com>
2026-04-21 16:34:00 +0600

    refactor: convert Message to JPA entity

--- src/main/java/kg/attractor/jobsearch/model/Message.java
+++ src/main/java/kg/attractor/jobsearch/model/Message.java
@@ -1,5 +1,6 @@
 package kg.attractor.jobsearch.model;
 
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -9,11 +10,20 @@ import java.time.LocalDateTime;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@Entity
+@Table(name = "messages")
 public class Message {
-
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    private Long respondedApplicants;
-    private String content;
-    private LocalDateTime timestamp;
 
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "response_id", nullable = false)
+    private RespondedApplicant response;
+
+    @Column(name = "content", nullable = false, length = 1000)
+    private String content;
+
+    @Column(name = "timestamp", nullable = false)
+    private LocalDateTime timestamp;
 }
