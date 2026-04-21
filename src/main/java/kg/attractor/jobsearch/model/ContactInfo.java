497e8b1c88b4274825c2cbf11ab6ede351316e5f
Kaniblanka <kanyshai.sydygalieva01@gmail.com>
2026-04-21 16:33:02 +0600

    refactor: convert ContactInfo to JPA entity

--- src/main/java/kg/attractor/jobsearch/model/ContactInfo.java
+++ src/main/java/kg/attractor/jobsearch/model/ContactInfo.java
@@ -1,5 +1,6 @@
 package kg.attractor.jobsearch.model;
 
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -7,11 +8,21 @@ import lombok.NoArgsConstructor;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@Entity
+@Table(name = "contact_info")
 public class ContactInfo {
-
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    private Long typeId;
-    private Long resumeId;
-    private String value;
 
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "type_id", nullable = false)
+    private ContactType type;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "resume_id", nullable = false)
+    private Resume resume;
+
+    @Column(name = "value", nullable = false)
+    private String value;
 }
