75339ca174ee560861c3050f8c060e2c83ed4d0f
Kaniblanka <kanyshai.sydygalieva01@gmail.com>
2026-04-23 17:14:24 +0600

    refactor: add pageable methods to vacancy service

--- src/main/java/kg/attractor/jobsearch/service/VacancyService.java
+++ src/main/java/kg/attractor/jobsearch/service/VacancyService.java
@@ -5,16 +5,22 @@ import kg.attractor.jobsearch.exception.CreateEntryException;
 import kg.attractor.jobsearch.exception.DeleteEntryException;
 import kg.attractor.jobsearch.exception.UpdateEntryException;
 import kg.attractor.jobsearch.exception.VacancyNotFoundException;
-
-import java.util.List;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 public interface VacancyService {
+
     VacancyDto createVacancy(VacancyDto vacancyDto) throws CreateEntryException;
+
     VacancyDto updateVacancy(Long id, VacancyDto vacancyDto) throws VacancyNotFoundException, UpdateEntryException;
+
     void deleteVacancy(Long id) throws VacancyNotFoundException, DeleteEntryException;
-    List<VacancyDto> getAllActiveVacancies();
-    List<VacancyDto> getVacanciesByCategory(Long categoryId);
+
+    Page<VacancyDto> getAllActiveVacancies(Pageable pageable, String sort);
+
+    Page<VacancyDto> getVacanciesByCategory(Long categoryId, Pageable pageable);
+
     VacancyDto getVacancyById(Long id) throws VacancyNotFoundException;
 
-    List<VacancyDto> getVacanciesByAuthorId(Long authorId);
+    Page<VacancyDto> getVacanciesByAuthorId(Long authorId, Pageable pageable, String sort);
 }
